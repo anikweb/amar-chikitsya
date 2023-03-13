1a3a7d39eecacf2722253cfd21c6db2a12521665
Anik Kumar Nandi <82234054+anikweb@users.noreply.github.com>
2023-03-13 07:31:18 +0600

    corona symptoms test checkbox

--- app/src/main/java/com/example/amarchikitsya/fragment/CoronaSymptomTestFragment.java
+++ app/src/main/java/com/example/amarchikitsya/fragment/CoronaSymptomTestFragment.java
@@ -3,6 +3,7 @@ package com.example.amarchikitsya.fragment;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -22,6 +23,22 @@ public class CoronaSymptomTestFragment extends Fragment {
 	                         Bundle savedInstanceState) {
 		binding = FragmentCoronaSymptomTestBinding.inflate(inflater);
 		
+		binding.coronaQueAnsYes.setOnClickListener(view -> {
+			binding.coronaQueAnsYesCheck.setChecked(true);
+			binding.coronaQueAnsNoCheck.setChecked(false);
+			binding.coronaQueAnsYesConst.setBackgroundResource(R.drawable.que_strock_selected);
+			binding.coronaQueAnsNoConst.setBackgroundResource(R.drawable.que_strock);
+			binding.coronaQueAnsYesText.setTextColor(ContextCompat.getColor(getActivity(),R.color.white));
+			binding.coronaQueNoText.setTextColor(ContextCompat.getColor(getActivity(),R.color.black));
+		});
+		binding.coronaQueAnsNo.setOnClickListener(view -> {
+			binding.coronaQueAnsNoCheck.setChecked(true);
+			binding.coronaQueAnsYesCheck.setChecked(false);
+			binding.coronaQueAnsNoConst.setBackgroundResource(R.drawable.que_strock_selected);
+			binding.coronaQueAnsYesConst.setBackgroundResource(R.drawable.que_strock);
+			binding.coronaQueNoText.setTextColor(ContextCompat.getColor(getActivity(),R.color.white));
+			binding.coronaQueAnsYesText.setTextColor(ContextCompat.getColor(getActivity(),R.color.black));
+		});
 		
 		
 		return binding.getRoot();
